e27970932fc0bb2a1e017463e787a4a8ae8be488
V.flower <superbcoder@icloud.com>
2020-07-21 01:24:02 +0300

    ready for ios
    ready for ios

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # xamarin-chat-sdk
 Voice &amp; Video Calling and Text Chat SDK for Xamarin
+
+## iOS
+
+Step 1: Create a blank project “SampleApp.Native.iOS”
+
+Step 2: Add CometChatPro SDK to References
+
+<img src="https://firebasestorage.googleapis.com/v0/b/imagecontents.appspot.com/o/ios_des_1.png?alt=media&token=210e36cb-7fc1-4592-a1e8-72744f755764" alt="Screenshot of the Step2"/>
+
+Step 3: Now you can use everything on CometChatPro SDK.
+
+<img src="https://firebasestorage.googleapis.com/v0/b/imagecontents.appspot.com/o/ios_des_2.png?alt=media&token=3e76acb5-ad7c-4ffa-a64e-bf594e9f7777" alt="Screenshot of the Step2"/>
+
---
+++ SampleApp.Native.iOS/Main.cs
@@ -0,0 +1,26 @@
+﻿using UIKit;
+using CometChatPro;
+
+namespace SampleApp.Native.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+
+            var appSettings = (new AppSettingsBuilder()).GetSubscribePresenceForAllUsers().SetRegionWithRegion("us").Build();
+
+            new CometChat("211751a156c3006", appSettings, (bool success) =>
+            {
+            }, (CometChatException error) =>
+            {
+
+            });
+
+        }
+    }
+}
